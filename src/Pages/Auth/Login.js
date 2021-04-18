22b8205e3ce2f38143e30414b5e08bcc0c780dec
unknown <regitiny@gmail.com>
2021-04-19 02:44:53 +0700

    add firebase

--- src/Pages/Auth/Login.js
+++ src/Pages/Auth/Login.js
@@ -1,18 +1,49 @@
 import './index.scss';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
 import firebase from 'firebase';
 
 // Configure Firebase.
 const config = {
-    apiKey: 'AIzaSyAeue-AsYu76MMQlTOM-KlbYBlusW9c1FM',
-    authDomain: 'myproject-1234.firebaseapp.com',
+    apiKey: 'AIzaSyDM8BWZASPQwX7k2gnar5GnALuwLYxRlY4',
+    authDomain: 'basic-shop-first-deploy.herokuapp.com',
     // ...
 };
 firebase.initializeApp(config);
 
-function Login(props) {
+const uiConfig = {
+    // Popup signin flow rather than redirect flow.
+    signInFlow: 'popup',
+    // We will display Google and Facebook as auth providers.
+    signInOptions: [
+        firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+        firebase.auth.FacebookAuthProvider.PROVIDER_ID
+    ],
+    callbacks: {
+        // Avoid redirects after sign-in.
+        signInSuccessWithAuthResult: () => false,
+    },
+};
 
+function Login(props) {
+    const [isSignedIn, setIsSignedIn] = useState(false);
+    useEffect(() => {
+        const unregisterAuthObserver = firebase.auth().onAuthStateChanged(async (user) => {
+            setIsSignedIn(!!user);
+            const token = await user.getIdToken();
+        });
+
+        return () => unregisterAuthObserver(); // Make sure we un-register Firebase observers when the component unmounts.
+    }, []);
+    if (!isSignedIn) {
+        return (
+            <div>
+                <h1>My App</h1>
+                <p>Please sign-in:</p>
+                <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
+            </div>
+        );
+    }
     return (
         <div className="login">
             <div className="form-login">
@@ -28,6 +59,10 @@ function Login(props) {
                 </form>
                 <p>Bạn chưa có tài khoản <strong>Đăng ký</strong></p>
                 <p>or login with social account</p>
+                <div>
+                    <p>Welcome {firebase.auth().currentUser.displayName}! You are now signed-in!</p>
+                    <a onClick={() => firebase.auth().signOut()}>Sign-out</a>
+                </div>
             </div>
         </div>
     );
